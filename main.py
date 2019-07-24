f8fdf1e206375497b1f9810de6529a514762946e
ryan <ryanmoodie@gmail.com>
2019-07-24 19:43:32 +0100

    Started separating logic into different classes

--- main.py
+++ main.py
@@ -11,11 +11,27 @@ plant_image = pyglet.resource.image("plant.png")
 plant_image.anchor_x = plant_image.width / 2
 
 
-class GameWindow(pyglet.window.Window):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.game_speed = 10
+class Chronometer:
+    def __init__(self):
+        self.time = 0  # total minutes
 
+    def get_day(self):
+        return self.time / (60 * 24)
+
+    def get_hour(self):
+        return (self.time % (60 * 24)) / 60
+
+    def get_minute(self):
+        return self.time % 60
+
+    @staticmethod
+    def format_time(time_quantity):
+        the_time = int(time_quantity)
+        return the_time if the_time > 9 else f"0{the_time}"
+
+
+class Plant:
+    def __init__(self):
         self.leaf_cost_atp = 3
         self.leaf_cost_water = 3
         self.root_cost_atp = 2
@@ -29,7 +45,20 @@ class GameWindow(pyglet.window.Window):
         self.leaves = 1
         self.roots = 1
 
-        self.time = 0  # total minutes
+    def set_growth_speed(self):
+        self.growth_speed = self.leaves
+
+    def set_water_speed(self):
+        self.water_speed = self.roots
+
+
+class GameWindow(pyglet.window.Window):
+    def __init__(self, timer, plant, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.timer = timer
+        self.plant = plant
+
+        self.game_speed = 10
 
         self.game_batch = pyglet.graphics.Batch()
         self.background = pyglet.graphics.OrderedGroup(0)
@@ -47,45 +76,25 @@ class GameWindow(pyglet.window.Window):
                                             y=self.row_1_height, group=self.foreground)
         self.time_text = pyglet.text.Label(text="", batch=self.game_batch, x=self.width * 5 / self.row_1_length,
                                            y=self.row_1_height, group=self.foreground)
-        self.plant = pyglet.sprite.Sprite(img=plant_image, x=self.width / 2, y=0, batch=self.game_batch,
-                                          group=self.background)
-        self.plant.scale = 0.1
+        self.plant_sprite = pyglet.sprite.Sprite(img=plant_image, x=self.width / 2, y=0, batch=self.game_batch,
+                                                 group=self.background)
+        self.plant_sprite.scale = 0.1
 
     def set_atp(self):
-        self.atp_text.text = f"ATP: {int(self.atp)}"
+        self.atp_text.text = f"ATP: {int(self.plant.atp)}"
 
     def set_water(self):
-        self.water_text.text = f"Water: {int(self.water)}"
+        self.water_text.text = f"Water: {int(self.plant.water)}"
 
     def set_leaves(self):
-        self.leaves_text.text = f"Leaves: {self.leaves}"
+        self.leaves_text.text = f"Leaves: {self.plant.leaves}"
 
     def set_roots(self):
-        self.roots_text.text = f"Roots: {self.roots}"
-
-    def get_day(self):
-        return self.time / (60 * 24)
-
-    def get_hour(self):
-        return (self.time % (60 * 24)) / 60
-
-    def get_minute(self):
-        return self.time % 60
-
-    @staticmethod
-    def format_time(time_quantity):
-        the_time = int(time_quantity)
-        return the_time if the_time > 9 else f"0{the_time}"
+        self.roots_text.text = f"Roots: {self.plant.roots}"
 
     def set_time(self):
-        self.time_text.text = \
-            f"Time: {int(self.get_day())}.{self.format_time(self.get_hour())}:{self.format_time(self.get_minute())}"
-
-    def set_growth_speed(self):
-        self.growth_speed = self.leaves
-
-    def set_water_speed(self):
-        self.water_speed = self.roots
+        self.time_text.text = f"Time: {int(self.timer.get_day())}.{self.timer.format_time(self.timer.get_hour())}:" \
+            f"{self.timer.format_time(self.timer.get_minute())}"
 
     def on_draw(self):
         game.clear()
@@ -99,42 +108,46 @@ class GameWindow(pyglet.window.Window):
 
     def on_key_press(self, symbol, modifiers):
         if symbol is pyglet.window.key.L:
-            if all([self.atp >= self.leaf_cost_atp, self.water >= self.leaf_cost_water]):
-                self.atp -= self.leaf_cost_atp
-                self.water -= self.leaf_cost_water
-                self.leaves += 1
-                self.plant.scale += 0.1
-            if self.atp < self.leaf_cost_atp:
+            if all([self.plant.atp >= self.plant.leaf_cost_atp, self.plant.water >= self.plant.leaf_cost_water]):
+                self.plant.atp -= self.plant.leaf_cost_atp
+                self.plant.water -= self.plant.leaf_cost_water
+                self.plant.leaves += 1
+                self.plant_sprite.scale += 0.1
+            if self.plant.atp < self.plant.leaf_cost_atp:
                 self.atp_text.color = (242, 38, 19, 255)
                 pyglet.clock.schedule_once(self.set_atp_white, .5)
-            if self.water < self.leaf_cost_water:
+            if self.plant.water < self.plant.leaf_cost_water:
                 self.water_text.color = (242, 38, 19, 255)
                 pyglet.clock.schedule_once(self.set_water_white, .5)
 
         if symbol is pyglet.window.key.R:
-            if self.atp >= self.root_cost_atp:
-                self.atp -= self.root_cost_atp
-                self.roots += 1
+            if self.plant.atp >= self.plant.root_cost_atp:
+                self.plant.atp -= self.plant.root_cost_atp
+                self.plant.roots += 1
             else:
                 self.atp_text.color = (242, 38, 19, 255)
                 pyglet.clock.schedule_once(self.set_atp_white, .5)
 
 
-game = GameWindow(width=WIDTH, height=HEIGHT)
+chronometer = Chronometer()
+the_plant = Plant()
+game = GameWindow(timer=chronometer, plant=the_plant, width=WIDTH, height=HEIGHT)
 
 
 def update(dt):
-    game.set_growth_speed()
-    game.set_water_speed()
+    the_plant.set_growth_speed()
+    the_plant.set_water_speed()
+
     game.set_atp()
     game.set_water()
     game.set_leaves()
     game.set_roots()
     game.set_time()
 
-    game.atp += game.growth_speed / UPS
-    game.water += game.water_speed / UPS
-    game.time += game.game_speed / UPS
+    the_plant.atp += the_plant.growth_speed / UPS
+    the_plant.water += the_plant.water_speed / UPS
+
+    chronometer.time += game.game_speed / UPS
 
 
 if __name__ == "__main__":
